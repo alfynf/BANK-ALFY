ac3de558a45568461067f8d39e7b2382887a0ee5
alfynf <alfynurf36@gmail.com>
2022-01-22 18:56:22 +0700

    add alamat to model

--- models/customer_models.go
+++ models/customer_models.go
@@ -8,6 +8,7 @@ type Nasabah struct {
 	gorm.Model
 	NoKTP        string `gorm:"type:varchar(16);unique;not null" json:"no_ktp" form:"no_ktp"`
 	Nama         string `gorm:"type:varchar(255);not null" json:"nama" form:"nama"`
+	Alamat       string `gorm:"type:varchar(255);not null" json:"alamat" form:"alamat"`
 	Telp         string `gorm:"type:varchar(50);unique;not null" json:"telp" form:"telp"`
 	TempatLahir  string `gorm:"type:varchar(255);not null" json:"tempat_lahir" form:"tempat_lahir"`
 	TanggalLahir string `gorm:"type:datetime;not null" json:"tanggal_lahir" form:"tanggal_lahir"`
@@ -18,9 +19,10 @@ type Body struct {
 }
 
 type GetResponse struct {
-	NoKTP        string `gorm:"type:varchar(16);unique;not null" json:"no_ktp" form:"no_ktp"`
-	Nama         string `gorm:"type:varchar(255);not null" json:"nama" form:"nama"`
-	Telp         string `gorm:"type:varchar(50);unique;not null" json:"telp" form:"telp"`
-	TempatLahir  string `gorm:"type:varchar(255);not null" json:"tempat_lahir" form:"tempat_lahir"`
-	TanggalLahir string `gorm:"type:datetime;not null" json:"tanggal_lahir" form:"tanggal_lahir"`
+	NoKTP        string `json:"no_ktp" form:"no_ktp"`
+	Nama         string `json:"nama" form:"nama"`
+	Alamat       string `json:"alamat" form:"alamat"`
+	Telp         string `json:"telp" form:"telp"`
+	TempatLahir  string `json:"tempat_lahir" form:"tempat_lahir"`
+	TanggalLahir string `json:"tanggal_lahir" form:"tanggal_lahir"`
 }

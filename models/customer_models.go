b85b50eb05c903eadb21ff85c6fd507317c77d50
alfynf <alfynurf36@gmail.com>
2022-01-24 16:56:10 +0700

    add golongan darah

--- models/customer_models.go
+++ models/customer_models.go
@@ -6,12 +6,13 @@ import (
 
 type Nasabah struct {
 	gorm.Model
-	NoKTP        string `gorm:"type:varchar(16);unique;not null" json:"no_ktp" form:"no_ktp"`
-	Nama         string `gorm:"type:varchar(255);not null" json:"nama" form:"nama"`
-	Alamat       string `gorm:"type:varchar(255);not null" json:"alamat" form:"alamat"`
-	Telp         string `gorm:"type:varchar(50);unique;not null" json:"telp" form:"telp"`
-	TempatLahir  string `gorm:"type:varchar(255);not null" json:"tempat_lahir" form:"tempat_lahir"`
-	TanggalLahir string `gorm:"type:datetime;not null" json:"tanggal_lahir" form:"tanggal_lahir"`
+	NoKTP         string `gorm:"type:varchar(16);unique;not null" json:"no_ktp" form:"no_ktp"`
+	Nama          string `gorm:"type:varchar(255);not null" json:"nama" form:"nama"`
+	Alamat        string `gorm:"type:varchar(255);not null" json:"alamat" form:"alamat"`
+	Telp          string `gorm:"type:varchar(50);unique;not null" json:"telp" form:"telp"`
+	TempatLahir   string `gorm:"type:varchar(255);not null" json:"tempat_lahir" form:"tempat_lahir"`
+	TanggalLahir  string `gorm:"type:datetime;not null" json:"tanggal_lahir" form:"tanggal_lahir"`
+	GolonganDarah string `gorm:"type:varchar(5);not null" json:"golongan_darah" form:"darah"`
 }
 
 // Body json untuk fitur yang meminta param no ktp
@@ -25,10 +26,11 @@ type BodyUpdate struct {
 }
 
 type GetResponse struct {
-	NoKTP        string `json:"no_ktp" form:"no_ktp"`
-	Nama         string `json:"nama" form:"nama"`
-	Alamat       string `json:"alamat" form:"alamat"`
-	Telp         string `json:"telp" form:"telp"`
-	TempatLahir  string `json:"tempat_lahir" form:"tempat_lahir"`
-	TanggalLahir string `json:"tanggal_lahir" form:"tanggal_lahir"`
+	NoKTP         string `json:"no_ktp" form:"no_ktp"`
+	Nama          string `json:"nama" form:"nama"`
+	Alamat        string `json:"alamat" form:"alamat"`
+	Telp          string `json:"telp" form:"telp"`
+	TempatLahir   string `json:"tempat_lahir" form:"tempat_lahir"`
+	TanggalLahir  string `json:"tanggal_lahir" form:"tanggal_lahir"`
+	GolonganDarah string `json:"golongan_darah" form:"darah"`
 }
